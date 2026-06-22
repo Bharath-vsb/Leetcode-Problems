252973d6e0f2ae8fafaf612823f0f044109b75b3
Bharath V <vbharathvsb@gmail.com>
2026-06-22 15:25:27 +0530

    Time: 1 ms (88.65%), Space: 44 MB (87.27%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,15 +1,19 @@
 class Solution {
+
+    public static void backtrack(int[] nums,int start, List<List<Integer>> res, List<Integer>temp)
+    {
+        res.add(new ArrayList<>(temp));
+        for(int i=start;i<nums.length;i++){
+            temp.add(nums[i]);
+            backtrack(nums,i+1,res,temp);
+            temp.remove(temp.size()-1);
+        }
+
+    }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>>res =new ArrayList<>();
-        res.add(new ArrayList<>());
-        for(int num:nums){
-            int size=res.size();
-            for(int i=0;i<size;i++){
-                List<Integer>temp=new ArrayList<>(res.get(i));
-                temp.add(num);
-                res.add(temp);
-            }
-        }
+        List<Integer> temp=new ArrayList<>();
+        backtrack(nums,0,res,temp);
         return res;
     }
 }
